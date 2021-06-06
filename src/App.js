1f4d90fd34ee92a904b8a7ef3713c73edb273d5e
paymaan <paymankazemipymkz@gmail.com>
2021-06-06 10:07:21 +0430

    type-of-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Product from './components/Product/Product'
 
-const App = () => {
-  return (
-      <div id="main" className="container">
-          <h2>React App</h2>
-          <Product />
-      </div>
-  )
+class App extends React.Component {
+    render(){
+    return (
+       <div id="main" className="container">
+           <h2>React App</h2>
+           <Product />
+           <Product />
+           <Product />
+           <Product />
+       </div>
+   )
+    }
 }
 
 

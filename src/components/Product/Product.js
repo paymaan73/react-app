0efeda50619d56bc4b02176d28185d15a98bc0ad
paymaan <paymankazemipymkz@gmail.com>
2021-06-06 10:11:24 +0430

    dynamic-data

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 
 const Product = () => {
-    return <div>Product Name: Book</div>
+    const price = 89
+    return(
+        <div>
+          <p>Product Name: Book</p>
+          <p>Product Price: {price}</p>
+        </div>
+    )
 }
 
 export default Product
